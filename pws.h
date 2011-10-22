4c5d7fe7b5df5f048571f1a489ed694d8e020934
Noa Resare <noa@spotify.com>
2011-10-22 13:12:58 +0200

    Added GPLv3 license, updated Copyright statement to match GPLv3 template, wrote README

--- pws.h
+++ pws.h
@@ -1,4 +1,20 @@
-/* Copyright © 2011 Noa Resare */
+/* 
+ libpwsafe - a portable implementation of the passwordsafe format
+ Copyright © 2011 Noa Resare 
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 typedef struct pws_field {
     int type;
